6da2b7127269f7681dc65e1bf77e0a4246604741
frozar <fabien.rozar@gmail.com>
2022-02-14 15:33:22 +0400

    Implémentation de refresh

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const tickets = [];
 let id = 0;
 
-const button_ask_help = document.getElementById("ask_help");
-
 function computeFormattedDate(date) {
   function padding(x) {
     return String(x).padStart(2, "0");
@@ -13,6 +11,16 @@ function computeFormattedDate(date) {
   )}/${date.getFullYear()}, ${padding(date.getHours())}:${padding(
     date.getMinutes()
   )}:${padding(date.getSeconds())}`;
+}
+
+function passedTicketById(current_id) {
+  for (let i = 0; i < tickets.length; ++i) {
+    const current_ticket = tickets[i];
+    if (current_ticket[0] === current_id) {
+      current_ticket[3] = "passed";
+      break;
+    }
+  }
 }
 
 function addNewLine(ticket) {
@@ -34,17 +42,11 @@ function addNewLine(ticket) {
   button.value = id;
 
   button.addEventListener("click", (event) => {
-    console.dir(event);
-    console.dir(event.target);
-    console.log("value", event.target.value);
+    // console.dir(event);
+    // console.dir(event.target);
+    // console.log("value", event.target.value);
     const current_id = parseInt(event.target.value);
-    for (let i = 0; i < tickets.length; ++i) {
-      const current_ticket = tickets[i];
-      if (current_ticket[0] === current_id) {
-        current_ticket[3] = "passed";
-        break;
-      }
-    }
+    passedTicketById(current_id);
 
     renderTable(tickets);
   });
@@ -57,8 +59,6 @@ function addNewLine(ticket) {
   tr.appendChild(td_1);
   tr.appendChild(td_2);
 
-  console.log("state", state);
-  console.log("state === passed", state === "passed");
   tr.classList.add(["line"]);
 
   const table = document.getElementById("table");
@@ -110,6 +110,9 @@ function renderTable(tickets) {
 //   Récupérer la date et l'heure du moment où l'utilisateur à clicker sur le bouton : OK
 //   Ajouter la valeur récupérée, la date et l'heure du click et un
 //    bouton "je passe mon tour" dans un tableau visible par l'utilisateur
+
+const button_ask_help = document.getElementById("ask_help");
+
 button_ask_help.addEventListener("click", () => {
   const input_name = document.getElementById("name");
   const input_text = input_name.value;
@@ -125,6 +128,34 @@ button_ask_help.addEventListener("click", () => {
   renderTable(tickets);
 });
 
-// renderTable([["totoaaaa", new Date()]]);
-addNewTicket("totoaaaa");
+addNewTicket("toto");
+addNewTicket("tata");
+addNewTicket("tutu");
+addNewTicket("t");
+passedTicketById(1);
+passedTicketById(2);
 renderTable(tickets);
+
+const button_refresh = document.getElementById("refresh");
+
+button_refresh.addEventListener("click", () => {
+  console.log("refresh");
+
+  const indexes_to_delete = [];
+  for (let i = 0; i < tickets.length; ++i) {
+    const current_ticket = tickets[i];
+    const state = current_ticket[3];
+    if (state === "passed") {
+      indexes_to_delete.push(i);
+    }
+  }
+
+  indexes_to_delete.reverse();
+  for (let i = 0; i < indexes_to_delete.length; ++i) {
+    const index = indexes_to_delete[i];
+    tickets.splice(index, 1);
+  }
+
+  // Rendu du tableau dans l'HTML à partir des données de 'tickets'
+  renderTable(tickets);
+});
